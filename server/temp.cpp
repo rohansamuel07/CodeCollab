e2800f0a77557c3736766a9e0b913de85a3cc00b
Rohan Samuel <rohansamuelx07@gmail.com>
2025-03-09 12:52:02 +0530

    completed day 6

--- server/temp.cpp
+++
@@ -1,3 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() { cout << "Hello, C++!\n"; return 0; }
--- server/utils/executeCode.js
+++ server/utils/executeCode.js
@@ -1,3 +1,4 @@
+/*
 const { exec } = require("child_process");
 const fs = require("fs");
 
@@ -39,6 +40,66 @@ const executeCode = (code, language, callback) => {
     } else {
       callback(stdout);
     }
+  });
+};
+
+module.exports = executeCode;
+*/
+
+const { exec } = require("child_process");
+const fs = require("fs");
+
+const executeCode = (code, language, callback) => {
+  let command;
+  let fileName;
+  let outputFiles = [];
+
+  switch (language) {
+    case "javascript":
+      command = `timeout 5s node -e "${code.replace(/"/g, '\\"')}"`;
+      break;
+
+    case "python":
+      fileName = "temp.py";
+      fs.writeFileSync(fileName, code);
+      command = `timeout 5s python3 ${fileName}`;
+      outputFiles.push(fileName);
+      break;
+
+    case "c":
+      fileName = "temp.c";
+      fs.writeFileSync(fileName, code);
+      command = `gcc ${fileName} -o temp.out && timeout 5s ./temp.out`;
+      outputFiles.push(fileName, "temp.out");
+      break;
+
+    case "cpp":
+      fileName = "temp.cpp";
+      fs.writeFileSync(fileName, code);
+      command = `g++ ${fileName} -o temp.out && timeout 5s ./temp.out`;
+      outputFiles.push(fileName, "temp.out");
+      break;
+
+    case "java":
+      const match = code.match(/public\s+class\s+(\w+)/);
+      if (!match) return callback("❌ Java class name not found!");
+
+      fileName = `${match[1]}.java`;
+      fs.writeFileSync(fileName, code);
+      command = `javac ${fileName} && timeout 5s java ${match[1]}`;
+      outputFiles.push(fileName, `${match[1]}.class`);
+      break;
+
+    default:
+      return callback("❌ Unsupported language");
+  }
+
+  exec(command, { timeout: 5000 }, (error, stdout, stderr) => {
+    outputFiles.forEach((file) => fs.unlinkSync(file, { force: true }));
+
+    if (error) return callback(`⚠ Error: ${error.message}`);
+    if (stderr) return callback(`⚠ Stderr: ${stderr}`);
+    callback(stdout);
   });
 };
 
